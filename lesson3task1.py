397155e334c5cccc99017a29b8c229dfda16a083
Михеевы <miheyj@gmail.com>
2022-04-10 23:00:23 +0300

    lesson3task3

--- lesson3task1.py
+++ lesson3task1.py
@@ -1,13 +1,14 @@
-def num_translate():
-    one = 'один'
-    two = 'два'
-    three = 'три'
-    four = 'четыре'
-    five = 'пять'
-    six = 'шесть'
-    seven = 'семь'
-    eight = 'восемь'
-    nine = 'девять'
-    ten = 'десять'
-    print()
-num_translate()
+number = {'zero': 'ноль',
+              'one': 'один',
+              'two': 'два',
+              'three': 'три',
+              'four': 'четыре',
+              'five': 'пять',
+              'six': 'шесть',
+              'seven': 'семь',
+              'eight': 'восемь',
+              'nine': 'девять',
+              'ten': 'десять'}
+def num_translate(english):
+    return number.get(english)
+print(num_translate(input('Введите числовое значение, по английски, от 0 до 10: ')))
